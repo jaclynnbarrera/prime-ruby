19c221994e4572b7bdcd186e3d462ec8ed750a6e
Jaclyn <jaclynnbarrera@gmail.com>
2021-01-14 22:23:44 -0500

    commit tree 75a4bdb9ff3a0002fdda10ff4ada1f58604c6631

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+
+def prime?(int)
+    if int < 0 || int == 0 || int == 1
+        return false
+    else 
+        (2..int-1).to_a.all? do |range|
+            int % range !=0
+        end 
+    end
+end
